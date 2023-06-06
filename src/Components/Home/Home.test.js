75ccedef59ff0eba0a39fb8bdadc06f3c297c7f6
Quaya Brown <technoquaya@gmail.com>
2023-06-06 12:13:41 -0400

    green test

--- src/Components/Home/Home.test.js
+++ src/Components/Home/Home.test.js
@@ -1,1 +1,23 @@
-describe 
+import { render, screen } from "@testing-library/react";
+import Home from "./Home";
+
+// Tested by Quaya
+describe("The Home Component", () => {
+    test("renders the following items: h1, img, about", () => {
+        render(<Home />);
+
+        const headerValue = screen.getByText('Home'); 
+        const pValue = screen.getByText('Blah')
+        const imageValue = screen.getByAltText('heroImage')
+        const buttonElement = screen.getByRole("button", {
+            name: /More Info/i,
+        });
+
+        expect(headerValue).toHaveTextContent('Home');
+        expect(buttonElement).toBeInTheDocument();
+        expect (imageValue).toBeInTheDocument()
+        expect(pValue).toHaveTextContent('Blah')
+    })
+})
+
+// Tested by Quaya
