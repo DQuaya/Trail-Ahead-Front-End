f1df0546356c3cee67a39d35e5622f8a1620fd35
Quaya Brown <technoquaya@gmail.com>
2023-06-06 10:47:04 -0400

    added some components

---
+++ src/Components/Button/Button.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../Button/Button.css'
+
+const Button = () => {
+  return (
+    <div>
+        <button class="but">Get Started</button>
+    </div>
+  )
+}
+
+export default Button
