d8a33b23c413cf3fc1613879a961adf6df90fd9e
Gerald Pinkins <geraldpinkinsjr@gmail.com>
2023-06-06 11:18:55 -0400

    inital commit

---
+++ src/Components/About/AboutPage.js
@@ -0,0 +1,24 @@
+
+import './AboutPage.css'
+
+function AboutPage() {
+  return (
+    <div className="text-container">
+      <div className="mission-box">
+        <h1> Mission</h1>
+        <span>
+          <p>
+            To foster healthy connections, new friendships, and shared memories
+            through outdoor adventures!
+          </p>
+          <span>Healthy Hiking</span>
+        </span>
+      </div>
+      <div className="impactBox">
+        <h2 className="impactHeader"> Make an Impact </h2>
+      </div>
+    </div>
+  );
+}
+
+export default AboutPage;
