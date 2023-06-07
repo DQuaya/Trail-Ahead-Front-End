805bb2b3e24b729f4d564e5f7eddbac515a575aa
Thanuseng Kevin Prachith <tkprachith@gmail.com>
2023-06-07 11:40:29 -0400

    Adds labels for input fields on sign-up form

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,27 +1,35 @@
-import React from 'react'
+import React from "react";
 
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 // import Signup from '../Signup/Signup'
 
-
 const Home = () => {
-  return (
-    <div id='main'>
-        <div className='header-heading'>
-            <h3>Trail Ahead offers a quick and easy way for </h3>
-            <h1><span>Hikers </span> to Meet <br/>Hikers</h1>
-            <p className='details'>Go checkout the avaliable Hikes</p>
-            <div className='header-btns'>
-            <Link to='/signup' className='logo' smooth={true} duration={2000}>
-              <a href='#' className='header-btn'>Join Now</a>
-            </Link> 
-          
+    return (
+        <div id="main">
+            <div className="header-heading">
+                <h3>Trail Ahead offers a quick and easy way for </h3>
+                <h1>
+                    <span>Hikers </span> to Meet <br />
+                    Hikers
+                </h1>
+                <p className="details">Go checkout the avaliable Hikes</p>
+                <div className="header-btns">
+                    <Link
+                        to="/signup"
+                        className="logo"
+                        smooth={true}
+                        duration={2000}
+                    >
+                        <a href="#" className="header-btn">
+                            Join Now
+                        </a>
+                    </Link>
+                </div>
+            </div>
         </div>
+    );
+};
 
-    </div>
-  )
-}
-
-export default Home
+export default Home;
 
 // Coded by Quaya
