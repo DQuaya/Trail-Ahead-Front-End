7d86bb7cac907f97be0e5a3a03e89fc151c16684
Aaliyah Russell <aaliyah_russell@yahoo.com>
2023-06-07 10:29:46 -0400

    small changes

--- src/Components/Signup/Signup.js
+++ src/Components/Signup/Signup.js
@@ -2,16 +2,16 @@ import './Signup.css';
 
 const Signup = () => {
     return (
-        <div className="container">
+        <div className="form-container">
             <form>
                 <h1><strong>Sign Up</strong></h1>
                 <p>It's quick and easy.</p>
                 <hr></hr>
-                <div className="input-boxes"></div>
+                
                 <input required placeholder="First name"></input>
                 <input required placeholder="Last name "></input>             
                 <input required placeholder="Email "></input>
-                <input required placeholder="Password "></input>
+                <input required placeholder="New Password "></input>
                 <input required placeholder="Confirm password "></input>
                 <input required placeholder="Age"></input>
                 <input placeholder="Sex"></input>
