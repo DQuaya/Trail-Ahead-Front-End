4763202124eda3b875d7569f8bc63e06022ffaad
Gerald Pinkins <geraldpinkinsjr@gmail.com>
2023-06-07 09:26:54 -0400

    initial layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Home from './Components/Home/Home';
+import AboutPage from './Components/About/AboutPage';
+import './Components/About/AboutPage.css'
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      {/* <Home/> */}
+   <AboutPage />
     </div>
   );
 }
--- src/Components/About/AboutPage.js
+++ src/Components/About/AboutPage.js
@@ -1,24 +1,46 @@
-
-import './AboutPage.css'
+import "./AboutPage.css";
 
 function AboutPage() {
   return (
-    <div className="text-container">
+    <div className="text-containers">
+      <div className="imgBack"></div>
+
       <div className="mission-box">
         <h1> Mission</h1>
-        <span>
-          <p>
-            To foster healthy connections, new friendships, and shared memories
-            through outdoor adventures!
-          </p>
-          <span>Healthy Hiking</span>
-        </span>
+
+        <p>
+          To foster healthy connections, new friendships, and shared memories
+          through outdoor adventures!
+        </p>
+        <span>Healthy Hiking</span>
       </div>
+
+      <br />
+      <br />
       <div className="impactBox">
+        <br />
+        <br />
         <h2 className="impactHeader"> Make an Impact </h2>
+        <p>
+          When you download Trail Head it makes a difference! % from your
+          download goes to one of our non-profit partners.{" "}
+        </p>
       </div>
+      <SectionContainer />
     </div>
   );
 }
+
+const SectionContainer = () => {
+  return (
+    <div className="section-container">
+      <div className="box"></div>
+      <div className="box"></div>
+      <div className="box"></div>
+      <div className="box"></div>
+      <div className="box"></div>
+    </div>
+  );
+};
 
 export default AboutPage;
