b4944e8e03ea8edf93be2ffc90d3c2e8c3178438
Aaliyah Russell <aaliyah_russell@yahoo.com>
2023-06-09 15:36:19 -0400

    Got stuff back AGAIN

--- src/Components/Hikedetails/Hikedetails.js
+++ src/Components/Hikedetails/Hikedetails.js
@@ -11,10 +11,10 @@ const Hikedetails = () => {
     <div className='hike-details'>
         <CoverImage/>
         <TrailName/>
-        <ArticleInfo />
-        <ProfilePix />
-        <WYSK />
-        <WYSKArt />
+        <ArticleInfo/>
+        <ProfilePix/>
+        <WYSK/>
+        <WYSKArt/>
     </div>
   )
 }
