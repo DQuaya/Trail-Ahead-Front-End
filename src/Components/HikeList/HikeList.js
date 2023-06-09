ad066035ea6ce6fc0dbcc851c9a341cac473e3fa
Quaya Brown <technoquaya@gmail.com>
2023-06-09 19:51:49 -0400

    created the profile page and finished making it responsive.

--- src/Components/HikeList/HikeList.js
+++ src/Components/HikeList/HikeList.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import coverImage from "../../assets/mountains.jpg";
+// import coverImage from "../../assets/mountains.jpg";
 import PlannedHike from "./PlannedHike";
 import './HikeList.css'
 
