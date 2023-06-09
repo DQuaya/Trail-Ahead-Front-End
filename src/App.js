7e9ec9e5c94e223e75c1383471c33d4301c45733
Gerald Pinkins <geraldpinkinsjr@gmail.com>
2023-06-09 15:23:20 -0400

    created quiz component and quiz route

--- src/App.js
+++ src/App.js
@@ -12,52 +12,54 @@ import HikeList from "./Components/HikeList/HikeList";
 import Profile from "./Components/Profile/Profile";
 // import Footer from "./Components/Footer/Footer";
 import Aboutsection from "./Components/Aboutsection/Aboutsection";
-
+import QuizForm from "./Components/Quiz/Quiz";
 
 function App() {
-    const [loading, setLoading] = useState(false);
-    const override = css`
-        display: block;
-        border-color: red;
-        margin-top: 20%;
-    `;
+  const [loading, setLoading] = useState(false);
+  const override = css`
+    display: block;
+    border-color: red;
+    margin-top: 20%;
+  `;
 
-    useEffect(() => {
-        setLoading(true);
-        setTimeout(() => {
-            setLoading(false);
-        }, 2000);
-    }, []);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
 
-    return (
-        <div className="App">
-            {loading ? (
-                <PropagateLoader
-                    color={"#f7792b"}
-                    Loading={loading}
-                    css={override}
-                    size={40}
-                />
-            ) : (
-                <>
-                    <Navbar />
-                    {/* <Home /> */}
-                    {/* <Testing/> */}
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="about" element={<AboutPage />} />
-                        <Route path="signup" element={<Signup />} />
-                        <Route path="login" element={<Login />} />
-                        {/* <Route path='hikes' element={<Hikes/>}/>
+  return (
+    <div className="App">
+      {loading ? (
+        <PropagateLoader
+          color={"#f7792b"}
+          Loading={loading}
+          css={override}
+          size={40}
+        />
+      ) : (
+        <>
+          <Navbar />
+          {/* <Home /> */}
+          {/* <Testing/> */}
+
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="about" element={<AboutPage />} />
+            <Route path="signup" element={<Signup />} />
+            <Route path="login" element={<Login />} />
+            {/* <Route path='hikes' element={<Hikes/>}/>
                     <Route path='profile' element={<Contact/>}/> */}
-                        <Route path="hike" element={<HikeList />} />
-                        <Route path="profile" element={<Profile />} />
-                    </Routes>
-                    
-                </>
-            )}
-        </div>
-    );
+            <Route path="hike" element={<HikeList />} />
+            <Route path="profile" element={<Profile />} />
+            <Route path="quiz" element={<QuizForm />} />
+          </Routes>
+        </>
+      )}
+      
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Components/Quiz/Quiz.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { useState } from "react";
+
+function QuizForm() {
+  const [question, setQuestion] = useState('');
+  const [answer, setAnswer] = useState('');
+
+
+ const handleQuestionChange = (event) => {
+   setQuestion(event.target.value);
+ };
+
+ const handleAnswerChange = (event) => {
+   setAnswer(event.target.value);
+ };
+
+ const handleSubmit = (event) => {
+   event.preventDefault();
+   // Do something with the question and answer values, such as saving them to a database
+   console.log("Question:", question);
+   console.log("Answer:", answer);
+   // Reset the form
+   setQuestion("");
+   setAnswer("");
+ };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Question:
+          <input type="text" value={question} onChange={handleQuestionChange} />
+        </label>
+        <br />
+        <label>
+          Answer:
+          <input type="text" value={answer} onChange={handleAnswerChange} />
+        </label>
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+  }
+
+export default QuizForm;
