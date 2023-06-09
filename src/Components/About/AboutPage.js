0576b4b987ef197f34b44d1320b2eb760fa9d9be
Gerald Pinkins <geraldpinkinsjr@gmail.com>
2023-06-09 11:16:24 -0400

    Data fetch for hikes working

--- src/Components/About/AboutPage.js
+++ src/Components/About/AboutPage.js
@@ -9,7 +9,7 @@ import {motion} from 'framer-motion'
 import { creatorsData } from "./creatorsData";
 import left from './left.png'
 import right from './right.png'
-import { Color } from "three";
+// import { Color } from "three";
 
 // Quaya coded this
 
