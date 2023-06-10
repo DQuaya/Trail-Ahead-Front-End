605c32f641e45a92d3d91535579d19ea436955c0
Quaya Brown <technoquaya@gmail.com>
2023-06-10 16:37:39 -0400

    footer is done and styled

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,30 +1,31 @@
 import React from "react";
 
 import { Link } from "react-router-dom";
+import './Home.css'
 import Aboutsection from "../Aboutsection/Aboutsection";
-
 
 const Home = () => {
 
   return (
+    <div className="Trying">
     <div id='main'>
-        <div className='header-heading'>
-            <h3>Trail Ahead offers a quick and easy way for </h3>
-            <h1><span>Hikers </span> to Meet <br/>Hikers</h1>
+      <div className='header-heading'>
+          <h3>Trail Ahead offers a quick and easy way for </h3>
+          <h1><span>Hikers </span> to Meet <br/>Hikers</h1>
             <p className='details'>Go checkout the avaliable Hikes</p>
-            <div className='header-btns'>
+          <div className='header-btns'>
             <Link to='/signup' className='logo' smooth={true} duration={2000}>
               <a href='#' className='header-btn'>Join Now</a>
             </Link> 
           </div>
-        </div>
-        <div className="about-section">
-
-         <Aboutsection/>
-
-         
-        </div>
+      </div>
+        
     </div>
+
+    <h1>About Trail Ahead</h1>
+      <Aboutsection/>
+    </div>
+    
   )
 }
 
