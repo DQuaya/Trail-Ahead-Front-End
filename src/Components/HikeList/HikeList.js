d792f1fd7ea44738941627681676fca76e76d337
Thanuseng Kevin Prachith <tkprachith@gmail.com>
2023-06-12 15:37:43 -0400

    Updates HikeList page to fetch data from backend

--- src/Components/HikeList/HikeList.js
+++ src/Components/HikeList/HikeList.js
@@ -1,38 +1,75 @@
 import { useState, useEffect } from "react";
-
-// import coverImage from "../../assets/mountains.jpg";
-
-import { redirect } from "react-router-dom";
-
 import PlannedHike from "./PlannedHike";
 import "./HikeList.css";
 
 const HikeList = () => {
-    const [hikes, setHikes] = useState([]); // when data is fetched from the backend, the setHikes function is used to store the data in hikes
+    const [hikes, setHikes] = useState([]);
+    const [users, setUsers] = useState([]);
+    const [hikers, setHikers] = useState([]);
 
-    useEffect(() => {
+    // defines functions to fetch all necessary information
+    const fetchHikeData = () => {
         fetch("http://localhost:3000/hikes")
             .then((response) => response.json())
             .then((data) => setHikes(data));
-    }, []);
-
-    const handleRouteChange = (id) => {
-        redirect(`/hikes/${id}`)
     };
+
+    const fetchUserData = () => {
+        fetch("http://localhost:3000/users")
+            .then((response) => response.json())
+            .then((data) => setUsers(data));
+    }
+
+    const fetchHikerData = () => {
+        fetch("http://localhost:3000/hikelist")
+        .then((response) => response.json())
+        .then((data) => setHikers(data));
+    }
+
+    function calculateGroupSize(hikeId) {
+        const currentGroupSize = hikers.filter(hike => hike.hikeId === hikeId)
+        return currentGroupSize.length
+    }
+
+    function findHikePlannerName(hikePlannerId) {
+        const hikePlanner = users.filter(user => user.id === hikePlannerId)
+        const name = `${hikePlanner[0].firstName} ${hikePlanner[0].lastName}`
+        return name
+    }
+
+    function findHikePlannerProfilePicture(hikePlannerId) {
+        const hikePlanner = users.filter(user => user.id === hikePlannerId)
+        const profilePicture = hikePlanner[0].profilePicture
+        return profilePicture
+    }
+    function findHikePlannerExperience(hikePlannerId) {
+        const hikePlanner = users.filter(user => user.id === hikePlannerId)
+        const experience = hikePlanner[0].experience
+        return experience
+    }
+
+    // on page load, fetches all data necessary to render HikeList
+    useEffect(() => {
+        fetchHikeData();
+        fetchUserData();
+        fetchHikerData();
+    }, []);
 
     return (
         <div className="hikeListMain">
             <h1>Available Hikes</h1>
+            <button onClick={fetchHikeData}>Refresh List</button>
             {hikes.map((hike) => (
                 <PlannedHike
-                    key={hike.name}
-                    name={hike.name}
-                    pictureAltText={hike.pictureAltText}
-                    experience={hike.experience}
-                    timeDate={hike.timeDate}
-                    groupSize={hike.groupSize}
-                    maxGroupSize={hike.maxGroupSize}
+                    key={hike.id}
+                    hikePlannerName={findHikePlannerName(hike.hikePlanner)}
+                    hikePlannerProfilePicture={findHikePlannerProfilePicture(hike.hikePlanner)}
+                    hikePlannerExperience={findHikePlannerExperience(hike.hikePlanner)}
                     hikeId={hike.id}
+                    trailName={hike.trailName}
+                    timeDate={hike.timeDate}
+                    currentGroupSize={calculateGroupSize(hike.id)}
+                    maxGroupSize={hike.maxGroupSize}
                 />
             ))}
         </div>
--- src/Components/HikeList/PlannedHike.js
+++ src/Components/HikeList/PlannedHike.js
@@ -1,44 +1,52 @@
 import "./PlannedHike.css";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 const PlannedHike = ({
-    name,
-    pictureSource,
-    pictureAltText,
-    experience,
+    hikePlannerName,
+    hikePlannerProfilePicture,
+    hikePlannerExperience,
+    trailName,
     timeDate,
-    groupSize,
+    currentGroupSize,
     maxGroupSize,
     hikeId,
 }) => {
     return (
         <div className="plannedHikeContainer">
-            <div>
-                <h3>{name}</h3>
-                <img src={pictureSource} alt={pictureAltText} />
-            </div>
-            <div>
-                <h3>Experience</h3>
-                <span>{experience}</span>
-            </div>
-            <div>
-                <h3>Time and Date</h3>
-                <span>{timeDate}</span>
-            </div>
-            <div>
-                <h3>Group Size</h3>
-                <span>
-                    {groupSize} / {maxGroupSize}
-                </span>
-            </div>
-            <div>
-                <h3>Availability</h3>
-                <span>{groupSize !== maxGroupSize ? "active" : "full"}</span>
-            </div>
-            <div>
-                <Link to={ (hikeId !== undefined) ? `/hike/${hikeId}` : `/hike` }>
-                    View Hike
-                </Link>
+            <h2>
+                {trailName} Hike on {timeDate}
+            </h2>
+            <div className="hikePlannerContainer">
+                <div>
+                    <h3>Organized by <span>{hikePlannerName}</span></h3>
+                    <img
+                        src={hikePlannerProfilePicture}
+                        alt={`Profile Picture of ${hikePlannerName}`}
+                    />
+                </div>
+                <div>
+                    <h3>Experience</h3>
+                    <span>{hikePlannerExperience}</span>
+                </div>
+                <div>
+                    <h3>Group Size</h3>
+                    <span>
+                        {currentGroupSize} / {maxGroupSize}
+                    </span>
+                </div>
+                <div>
+                    <h3>Availability</h3>
+                    <span>
+                        {currentGroupSize !== maxGroupSize ? "active" : "full"}
+                    </span>
+                </div>
+                <div>
+                    <Link
+                        to={hikeId !== undefined ? `/hike/${hikeId}` : `/hike`}
+                    >
+                        <span className="">View Hike</span>
+                    </Link>
+                </div>
             </div>
         </div>
     );
